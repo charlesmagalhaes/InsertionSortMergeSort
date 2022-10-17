1757ecd7fd75318b8605a68b7eda1fdddd5b27a3
Charles Magalhaes <charlesrm29@gmail.com>
2022-10-17 17:14:22 -0300

    Implementação dos métodos insertionSort e imprimirArray

--- src/AplicacaoSort.java
+++ src/AplicacaoSort.java
@@ -1,8 +1,26 @@
+import java.util.Random;
 
 public class AplicacaoSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MergeInsertionSort ordenar = new MergeInsertionSort();
+		
+		int[] lista;
+		int[] numArray = {12, 8, 18, 4, 13, 18, 0, 12, 6, 2, 0, 11, 10, 0, 4, 17, 19, 16, 20, 13}; 
+		
+		Random gerador = new Random();
+
+       
+        /*for (int i = 0; i < 20; i++) {
+           numArray[i] = gerador.nextInt(26);
+           System.out.print(numArray[i]+", ");
+         }
+		*/
+	
+		ordenar.insertionSort(numArray);
+		
+		ordenar.imprimirArray(numArray);
+		
 
 	}
 
--- src/MergeInsertionSort.java
+++ src/MergeInsertionSort.java
@@ -1,11 +1,31 @@
 
 public class MergeInsertionSort {
 	
+	
 	public void insertionSort(int[] lista) {
-		
+		int key;
+		int i;
+		for(int j=1; j< lista.length;j++) {
+			key = lista[j];
+			i = j -1;
+			
+			while (i >= 0  && lista[i] > key) {
+				lista[i+1] = lista[i];
+				i = i -1;
+			}
+			lista[i+1] = key;
+		}
 		
 	}
 	
-	
+	 public void imprimirArray(int[] lista)
+	    {
+		 System.out.print("Lista organizada: ");
+	        for (int i = 0; i < lista.length; ++i)
+	            System.out.print(lista[i] + " ");
+	  
+	        System.out.println();
+	    }
+	  
 
 }
