445fa939d11dee1c71b055ca33d7738b52c42132
Charles Magalhaes <charlesrm29@gmail.com>
2022-10-17 17:41:43 -0300

    Implementação dos métodos merge e sort

--- src/AplicacaoSort.java
+++ src/AplicacaoSort.java
@@ -5,8 +5,7 @@ public class AplicacaoSort {
 	public static void main(String[] args) {
 		MergeInsertionSort ordenar = new MergeInsertionSort();
 		
-		int[] lista;
-		int[] numArray = {12, 8, 18, 4, 13, 18, 0, 12, 6, 2, 0, 11, 10, 0, 4, 17, 19, 16, 20, 13}; 
+		int[] lista = {12, 8, 18, 4, 13, 18, 0, 12, 6, 2, 0, 11, 10, 0, 4, 17, 19, 16, 20, 13}; 
 		
 		Random gerador = new Random();
 
@@ -16,12 +15,20 @@ public class AplicacaoSort {
            System.out.print(numArray[i]+", ");
          }
 		*/
-		ordenar.imprimirArray(numArray, "Lista de entrada: ");
+		ordenar.imprimirArray(lista, "Lista de entrada: ");
 	
-		ordenar.insertionSort(numArray);
+		ordenar.insertionSort(lista);
 		
-		ordenar.imprimirArray(numArray, "Lista Organizada: ");
+		ordenar.imprimirArray(lista, "Lista Ordenada por insertion Sort: ");
 		
+		int[] lista2 = {12, 8, 18, 4, 13, 18, 0, 12, 6, 2, 0, 11, 10, 0, 4, 17, 19, 16, 20, 13}; 
+		
+		System.out.println("-----------------------------------------------------------");
+		
+		ordenar.imprimirArray(lista2, "Lista de entrada: ");
+		
+		ordenar.sort(lista2, 0, lista.length-1);
+		ordenar.imprimirArray(lista2, "Lista Ordenada por Merge Sort: ");
 
 	}
 
--- src/MergeInsertionSort.java
+++ src/MergeInsertionSort.java
@@ -26,6 +26,70 @@ public class MergeInsertionSort {
 	  
 	        System.out.println();
 	    }
+	 
+	 
+	 
+	 public void merge(int[] lista, int l, int m, int r)
+	    {
+	        
+	        int n1 = m - l + 1;
+	        int n2 = r - m;
+	 
+	      
+	        int L[] = new int[n1];
+	        int R[] = new int[n2];
+	 
+	       
+	        for (int i = 0; i < n1; ++i)
+	            L[i] = lista[l + i];
+	        for (int j = 0; j < n2; ++j)
+	            R[j] = lista[m + 1 + j];
+	 
+	      
+	        int i = 0, j = 0;
+	 
+	      
+	        int k = l;
+	        while (i < n1 && j < n2) {
+	            if (L[i] <= R[j]) {
+	            	lista[k] = L[i];
+	                i++;
+	            }
+	            else {
+	            	lista[k] = R[j];
+	                j++;
+	            }
+	            k++;
+	        }
+	 
+	      
+	        while (i < n1) {
+	        	lista[k] = L[i];
+	            i++;
+	            k++;
+	        }
+	 
+	       
+	        while (j < n2) {
+	        	lista[k] = R[j];
+	            j++;
+	            k++;
+	        }
+	    }
+	 
+	   
+	   public void sort(int[] lista, int l, int r)
+	    {
+	        if (l < r) {
+	           
+	            int m = l + (r - l) / 2;
+	 
+	            sort(lista, l, m);
+	            sort(lista, m + 1, r);
+	 
+	           merge(lista, l, m, r);
+	        }
+	    }
 	  
 
 }
